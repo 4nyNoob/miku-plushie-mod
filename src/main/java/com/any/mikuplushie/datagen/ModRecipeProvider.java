e5c03fe4fb827e9c04963bb4edad0e8c748025ef
any <aniziooliveira9@gmail.com>
2024-09-24 21:22:51 -0300

    1.20.1 backport

--- src/main/java/com/any/mikuplushie/datagen/ModRecipeProvider.java
+++ src/main/java/com/any/mikuplushie/datagen/ModRecipeProvider.java
@@ -5,22 +5,23 @@ import com.any.mikuplushie.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.block.Blocks;
-import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
 
 	public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-		super(output, registriesFuture);
+		super(output);
 	}
 
 	@Override
-	public void generate(RecipeExporter exporter) {
+	public void generate(Consumer<RecipeJsonProvider> exporter) {
 		ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.CANUDINHO)
 				.pattern("p")
 				.pattern("p")
--- src/main/java/com/any/mikuplushie/item/MikuPlushieBlockItem.java
+++ src/main/java/com/any/mikuplushie/item/MikuPlushieBlockItem.java
@@ -1,31 +1,49 @@
 package com.any.mikuplushie.item;
 
+import com.any.mikuplushie.MikuPlushie;
 import com.any.mikuplushie.ModSoundEvents;
 import com.any.mikuplushie.datagen.ModTagProvider;
 import net.minecraft.block.Block;
+import net.minecraft.client.item.TooltipContext;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.Equipment;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.text.Text;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public class MikuPlushieBlockItem extends BlockItem {
+public class MikuPlushieBlockItem extends BlockItem implements Equipment {
 
 	public MikuPlushieBlockItem(Block block, Settings settings) {
 		super(block, settings);
 	}
 
+
+
 	@Override
-	public void appendTooltip(ItemStack stack, TooltipContext context, List<Text> tooltip, TooltipType type) {
-		tooltip.add(Text.translatable("item." + stack.getItem().toString().replace(":", ".") + ".tooltip"));
-		super.appendTooltip(stack, context, tooltip, type);
+	public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+		tooltip.add(Text.translatable("item." + MikuPlushie.MOD_ID + "." + stack.getItem().toString() + ".tooltip"));
+		super.appendTooltip(stack, world, tooltip, context);
 	}
 
 	public static void PlayMikuSound(LivingEntity entity){
 		if (entity.getStackInHand(entity.getActiveHand()).isIn(ModTagProvider.BR_MIKU_ITEMS)){
 			entity.playSound(ModSoundEvents.DOR, 1f, 1);
 		}
+	}
+
+	@Override
+	public EquipmentSlot getSlotType() {
+		return EquipmentSlot.HEAD;
+	}
+
+	@Override
+	public SoundEvent getEquipSound() {
+		return ModSoundEvents.OIE;
 	}
 }
