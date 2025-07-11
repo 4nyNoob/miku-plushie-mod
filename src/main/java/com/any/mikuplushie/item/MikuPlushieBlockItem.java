2ef8d1ec81f3a6ea4d12bdfd4fc2390fd92ed7dc
4ny <113947854+4nyNoob@users.noreply.github.com>
2025-07-11 15:47:59 -0300

    1.21.1 fabric fixups

--- src/main/java/com/any/mikuplushie/item/MikuPlushieBlockItem.java
+++ src/main/java/com/any/mikuplushie/item/MikuPlushieBlockItem.java
@@ -61,19 +61,19 @@ public class MikuPlushieBlockItem extends BlockItem implements Equipment {
 		ItemStack stack = this.getDefaultStack();
 
 		if (stack.isIn(ModTagProvider.AIKO_PLUSH)) {
-			return RegistryEntry.of( ModSoundEvents.AIKO_EQUIP);
+			return RegistryEntry.of(ModSoundEvents.AIKO_EQUIP);
 		}
 		if (stack.isIn(ModTagProvider.TETO_PLUSH)) {
-			return RegistryEntry.of( ModSoundEvents.TETO_EQUIP);
+			return RegistryEntry.of(ModSoundEvents.TETO_EQUIP);
 		}
 		if (stack.isIn(ModTagProvider.AKITA_NERU_PLUSH)) {
-			return RegistryEntry.of( ModSoundEvents.AKITA_NERU_EQUIP);
+			return RegistryEntry.of(ModSoundEvents.AKITA_NERU_EQUIP);
 		}
 		if (stack.isIn(ModTagProvider.RIN_PLUSH)) {
-			return RegistryEntry.of( ModSoundEvents.RIN_EQUIP);
+			return RegistryEntry.of(ModSoundEvents.RIN_EQUIP);
 		}
 		if (stack.isIn(ModTagProvider.LEN_PLUSH)) {
-			return RegistryEntry.of( ModSoundEvents.LEN_EQUIP);
+			return RegistryEntry.of(ModSoundEvents.LEN_EQUIP);
 		}
 
 		return RegistryEntry.of(ModSoundEvents.MIKU_EQUIP);
