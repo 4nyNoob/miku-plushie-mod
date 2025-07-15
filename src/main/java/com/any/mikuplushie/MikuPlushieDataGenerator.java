1fee0d0c7c90873c59f8c785b1061fdaa8038098
any <aniziooliveira9@gmail.com>
2025-07-15 17:56:14 -0300

    1.7 update

--- src/main/java/com/any/mikuplushie/MikuPlushieDataGenerator.java
+++ src/main/java/com/any/mikuplushie/MikuPlushieDataGenerator.java
@@ -3,7 +3,6 @@ package com.any.mikuplushie;
 import com.any.mikuplushie.datagen.ModItemModelProvider;
 import com.any.mikuplushie.datagen.ModLootTableProvider;
 import com.any.mikuplushie.datagen.ModModelProvider;
-import com.any.mikuplushie.datagen.ModRecipeProvider;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
@@ -31,7 +30,7 @@ public class MikuPlushieDataGenerator {
 		generator.addProvider(event.includeClient(),new ModItemModelProvider(packOutput,existingFileHelper));
 		generator.addProvider(event.includeClient(),new ModModelProvider(packOutput,existingFileHelper));
 
-		generator.addProvider(event.includeServer(),new ModRecipeProvider(packOutput,lookupProvider));
+//		generator.addProvider(event.includeServer(),new ModRecipeProvider(packOutput,lookupProvider));
 
 		generator.addProvider(event.includeServer(),new LootTableProvider(packOutput, Collections.emptySet(), List.of(new LootTableProvider.SubProviderEntry(ModLootTableProvider::new, LootContextParamSets.BLOCK)),lookupProvider));
 
