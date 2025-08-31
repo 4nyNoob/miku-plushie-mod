98423dbfa4cfe84fc8cb9700496f09d949a2b1ab
any <aniziooliveira9@gmail.com>
2025-08-31 15:47:26 -0300

    update README.md

--- src/main/java/com/any/mikuplushie/ModItems.java
+++ src/main/java/com/any/mikuplushie/ModItems.java
@@ -6,7 +6,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.*;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.common.SimpleTier;
@@ -194,8 +193,8 @@ public class ModItems {
 
 	public static final DeferredItem<MikuPlushieBlockItem> MIKU_PLUSH_MONITORING = ITEMS.register("miku_plush_monitoring",
 			()->new MikuPlushieBlockItem(ModBlocks.MIKU_PLUSH_MONITORING.get()));
-    public static final DeferredItem<MikuPlushieBlockItem> KONOHA_PLUSH = ITEMS.register("konoha_plush",
-            ()->new MikuPlushieBlockItem(ModBlocks.KONOHA_PLUSH.get()));
+    public static final DeferredItem<PlushieBlockItem> KONOHA_PLUSH = ITEMS.register("konoha_plush",
+            ()->new PlushieBlockItem(ModBlocks.KONOHA_PLUSH.get()));
     public static final DeferredItem<LukaPlushieBlockItem> LUKA_PLUSH = ITEMS.register("luka_plush",
             ()->new LukaPlushieBlockItem(ModBlocks.LUKA_PLUSH.get()));
     public static final DeferredItem<TetoPlushieBlockItem> TETO_PLUSH_SOME_MORE_OF_THAT_SONG = ITEMS.register("teto_plush_some_more_of_that_song",
