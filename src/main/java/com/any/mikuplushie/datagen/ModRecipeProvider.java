842d66dc0ca0a3339e3d7bb910cb0003f3ba1043
any <aniziooliveira9@gmail.com>
2025-08-31 16:32:46 -0300

    1.21.1 fixes

--- src/main/java/com/any/mikuplushie/datagen/ModRecipeProvider.java
+++ src/main/java/com/any/mikuplushie/datagen/ModRecipeProvider.java
@@ -4,7 +4,7 @@ import com.any.mikuplushie.ModBlocks;
 import com.any.mikuplushie.ModItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
-import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Items;
@@ -12,16 +12,15 @@ import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
 
 public class ModRecipeProvider extends FabricRecipeProvider {
 
-	public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
-		super(output);
-	}
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
 
 	@Override
-	public void generate(Consumer<RecipeJsonProvider> exporter) {
+	public void generate(RecipeExporter exporter) {
         ShapedRecipeJsonBuilder.create(RecipeCategory.FOOD, ModItems.CANUDINHO)
             .pattern("p")
             .pattern("p")
