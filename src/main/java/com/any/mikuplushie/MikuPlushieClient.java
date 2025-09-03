ec6053bb69aa65486871c377afe1f1da6fa249ea
any <aniziooliveira9@gmail.com>
2025-09-03 18:37:18 -0300

    add teto plush lobster

--- src/main/java/com/any/mikuplushie/MikuPlushieClient.java
+++ src/main/java/com/any/mikuplushie/MikuPlushieClient.java
@@ -79,6 +79,7 @@ public class MikuPlushieClient implements ClientModInitializer {
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TETO_PLUSH_LIAR_DANCER, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TETO_PLUSH_WHATCHACALLITSNAME, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TETO_PLUSH_SOME_MORE_OF_THAT_SONG, RenderLayer.getCutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TETO_PLUSH_LOBSTER, RenderLayer.getCutout());
 
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.AKITA_NERU_PLUSH, RenderLayer.getCutout());
 		BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.AKITA_NERU_PLUSH_TAILS, RenderLayer.getCutout());
