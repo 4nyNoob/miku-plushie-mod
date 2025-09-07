ed09e09115bb4706bcf09a7ed9d655612086c597
any <aniziooliveira9@gmail.com>
2025-09-07 17:34:21 -0300

    upgraded teto voice to SynthV

--- src/main/java/com/any/mikuplushie/datagen/ModTagProvider.java
+++ src/main/java/com/any/mikuplushie/datagen/ModTagProvider.java
@@ -102,7 +102,7 @@ public class ModTagProvider extends FabricTagProvider.ItemTagProvider {
             .add(ModItems.TETO_PLUSH_LIAR_DANCER)
             .add(ModItems.TETO_PLUSH_WHATCHACALLITSNAME)
             .add(ModItems.TETO_PLUSH_SOME_MORE_OF_THAT_SONG)
-            .add(ModItems.TETO_PLUSH_LOBSTER)
+//            .add(ModItems.TETO_PLUSH_LOBSTER)
         ;
         getOrCreateTagBuilder(TETO_PICKAXE)
             .add(ModItems.TETO_PICKAXE)
