667e341da5d20f944e92e8ea82e76076270363fa
any <aniziooliveira9@gmail.com>
2025-10-24 21:35:31 -0300

    updated spawn_mikus command

--- src/main/java/com/any/mikuplushie/MikuPlushie.java
+++ src/main/java/com/any/mikuplushie/MikuPlushie.java
@@ -1,27 +1,10 @@
 package com.any.mikuplushie;
 
-import com.any.mikuplushie.datagen.ModTagProvider;
+import com.any.mikuplushie.commands.SpawnMikusCommand;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.command.argument.Vec3ArgumentType;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.decoration.ArmorStandEntity;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.Text;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 public class MikuPlushie implements ModInitializer {
 	public static final String MOD_ID = "miku-plushie";
@@ -40,115 +23,9 @@ public class MikuPlushie implements ModInitializer {
 		ModBlocks.initialize();
 		ModSoundEvents.initialize();
 
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            dispatcher
-                .register(CommandManager.literal("spawn_mikus")
-                .then(CommandManager.argument("pos", Vec3ArgumentType.vec3())
-                    .executes(context -> {
-
-                        //GLOBAL VARIABLES
-                        ServerWorld world = context.getSource().getWorld();
-                        Vec3d sourcePos = Vec3ArgumentType.getVec3(context, "pos");
-                        int spacing = 3;
-
-                        //GET PLUSHIES FROM ITEM TAG
-                        Registry<Item> registry = world.getRegistryManager().get(RegistryKeys.ITEM);
-                        List<ItemStack> PLUSHIES = new ArrayList<>();
-
-                        for (int item = 0; item < registry.size(); item++) {
-                            if (Objects.requireNonNull(registry.get(item)).getDefaultStack().isIn(ModTagProvider.PLUSHIES)){
-                                PLUSHIES.add(Objects.requireNonNull(registry.get(item)).getDefaultStack());
-                            }
-                        }
-
-                        //INITIAL ARMOR STAND POS
-                        BlockPos plushiesSpawnPos =
-                            BlockPos.ofFloored(
-                                sourcePos.x,
-                                sourcePos.y,
-                                sourcePos.z
-                            );
-
-                        //HANDLE PLUSHIES SPAWNING
-                        int plushies_rows = (int) Math.ceil(Math.sqrt(PLUSHIES.size()));
-                        int plushies_columns = 9;
-                        int plushies = 0;
-
-                        //ROWS SPAWN
-                        for (int r = 0; r < plushies_rows; r++) {
-                            //COLUMNS SPAWN
-                            for (int c = 0; c < plushies_columns; c++) {
-                                //AVOID SPAWNING MORE ARMOR STANDS THAN NECESSARY
-                                if (plushies < PLUSHIES.size()) {
-
-                                    ItemStack itemStack = PLUSHIES.get(plushies);
-
-                                    ArmorStandEntity armorStandEntity = new ArmorStandEntity(world,
-                                        plushiesSpawnPos.getX() + c * spacing + 0.5,
-                                        plushiesSpawnPos.getY(),
-                                        plushiesSpawnPos.getZ() + r * spacing + 0.5
-                                    );
-
-                                    armorStandEntity.equipStack(EquipmentSlot.HEAD, itemStack);
-                                    armorStandEntity.equipStack(EquipmentSlot.MAINHAND, itemStack);
-                                    armorStandEntity.equipStack(EquipmentSlot.OFFHAND, itemStack);
-
-                                    armorStandEntity.setShowArms(true);
-                                    armorStandEntity.setYaw(180F);
-                                    armorStandEntity.setNoGravity(true);
-                                    armorStandEntity.setCustomName(Text.of("Plush"));
-                                    world.spawnEntity(armorStandEntity);
-                                }
-                                plushies ++;
-                            }
-                        }
-
-                        List<ItemStack> PICKAXES = new ArrayList<>();
-
-                        for (int item = 0; item < registry.size(); item++) {
-                            if (Objects.requireNonNull(registry.get(item)).getDefaultStack().isIn(ModTagProvider.TETO_PICKAXE)){
-                                PICKAXES.add(Objects.requireNonNull(registry.get(item)).getDefaultStack());
-                            }
-                        }
-
-                        BlockPos pickaxeSpawnPos = plushiesSpawnPos.offset(Direction.UP, 5);
-
-                        //HANDLE PLUSHIES SPAWNING
-                        int pickaxesRows = (int) Math.ceil(Math.sqrt(PICKAXES.size()));
-                        int pickaxesColumns = 9;
-                        int pickaxe = 0;
-
-                        //ROWS SPAWN
-                        for (int r = 0; r < pickaxesRows; r++) {
-                            //COLUMNS SPAWN
-                            for (int c = 0; c < pickaxesColumns; c++) {
-                                //AVOID SPAWNING MORE ARMOR STANDS THAN NECESSARY
-                                if (pickaxe < PICKAXES.size()) {
-                                    ArmorStandEntity armorStandEntity = new ArmorStandEntity(world,
-                                        pickaxeSpawnPos.getX() + c * spacing + 0.5,
-                                        pickaxeSpawnPos.getY(),
-                                        pickaxeSpawnPos.getZ() + r * spacing + 0.5
-                                    );
-
-                                    armorStandEntity.equipStack(EquipmentSlot.HEAD, PICKAXES.get(pickaxe));
-                                    armorStandEntity.equipStack(EquipmentSlot.MAINHAND, PICKAXES.get(pickaxe));
-                                    armorStandEntity.equipStack(EquipmentSlot.OFFHAND, PICKAXES.get(pickaxe));
-
-                                    armorStandEntity.setShowArms(true);
-                                    armorStandEntity.setYaw(180F);
-                                    armorStandEntity.setNoGravity(true);
-                                    armorStandEntity.setCustomName(Text.of("Plush"));
-                                    world.spawnEntity(armorStandEntity);
-                                }
-                                pickaxe ++;
-                            }
-                        }
-
-                        return 1;
-                    }
-                )
-            ));
-        });
-
+        CommandRegistrationCallback.EVENT.register(((
+            commandDispatcher, commandRegistryAccess, registrationEnvironment) ->
+            SpawnMikusCommand.register(commandDispatcher))
+        );
 	}
 }
---
+++ src/main/java/com/any/mikuplushie/commands/SpawnMikusCommand.java
@@ -0,0 +1,114 @@
+package com.any.mikuplushie.commands;
+
+import com.any.mikuplushie.ModBlocks;
+import com.any.mikuplushie.datagen.ModTagProvider;
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.block.Block;
+import net.minecraft.command.argument.Vec3ArgumentType;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class SpawnMikusCommand {
+
+    public static void register (CommandDispatcher<ServerCommandSource> dispatcher) {
+        dispatcher.register(CommandManager.literal("spawn_mikus")
+            .then(CommandManager.argument("pos", Vec3ArgumentType.vec3())
+                .executes(commandContext ->
+                    spawnMikus(commandContext.getSource(),
+                        Vec3ArgumentType.getVec3(commandContext, "pos"))))
+        );
+    }
+
+    public static int spawnMikus(ServerCommandSource source, Vec3d posArgument) {
+        //GLOBAL VARIABLES
+        ServerWorld world = source.getWorld();
+        BlockPos spawnPos = BlockPos.ofFloored(posArgument);
+        int spacing = 3;
+
+        //CREATE LISTS FROM ITEM TAG
+        Registry<Item> itemRegistry = world.getRegistryManager().get(RegistryKeys.ITEM);
+        Registry<Block> blockRegistry = world.getRegistryManager().get(RegistryKeys.BLOCK);
+        List<ItemStack> PLUSHIES = new ArrayList<>();
+        List<ItemStack> PICKAXES = new ArrayList<>();
+
+        for (int item = 0; item < itemRegistry.size(); item++) {
+            if (Objects.requireNonNull(itemRegistry.get(item)).getDefaultStack().isIn(ModTagProvider.PLUSHIES)){
+                PLUSHIES.add(Objects.requireNonNull(itemRegistry.get(item)).getDefaultStack());
+            }
+            if (Objects.requireNonNull(itemRegistry.get(item)).getDefaultStack().isIn(ModTagProvider.TETO_PICKAXE)){
+                PICKAXES.add(Objects.requireNonNull(itemRegistry.get(item)).getDefaultStack());
+            }
+        }
+
+        List<Block> BLOCKS = new ArrayList<>();
+        for (int block = 0; block < blockRegistry.size(); block++) {
+            if (Objects.requireNonNull(blockRegistry.get(block)).asItem().getDefaultStack().isIn(ModTagProvider.PLUSHIES)){
+                BLOCKS.add(blockRegistry.get(block));
+            }
+        }
+
+        //LIST OF LISTS
+        List<List<?>> LISTS = List.of(
+            PLUSHIES,
+            PICKAXES,
+            BLOCKS
+        );
+
+        for (int list = 0; list < LISTS.size(); list++) {
+            //HANDLE PLUSHIES SPAWNING
+            List<?> currentList = LISTS.get(list);
+            int plushiesRows = (int) Math.ceil(Math.sqrt(currentList.size()));
+            int plushiesColumns = 9;
+            int plushies = 0;
+
+            //ROWS SPAWN
+            for (int r = 0; r < plushiesRows; r++) {
+                //COLUMNS SPAWN
+                for (int c = 0; c < plushiesColumns; c++) {
+                    //AVOID SPAWNING MORE ARMOR STANDS THAN NECESSARY
+                    if (plushies < currentList.size()) {
+                        //PLACE PLUSHIE BLOCKS
+                        if (currentList.contains(ModBlocks.MIKU_PLUSH_BR)){
+                            BlockPos blockPos = spawnPos.add(c * spacing, list * spacing + 1, r * spacing);
+                            world.setBlockState(blockPos, ((Block) currentList.get(plushies)).getDefaultState());
+                        }
+                        //CREATE AND SPAWN ARMOR STAND
+                        else  {
+                            ArmorStandEntity armorStandEntity = new ArmorStandEntity(world,
+                                spawnPos.getX() + c * spacing + 0.5,
+                                spawnPos.getY() + list * spacing,
+                                spawnPos.getZ() + r * spacing + 0.5
+                            );
+                            ItemStack itemStack = (ItemStack) currentList.get(plushies);
+                            armorStandEntity.equipStack(EquipmentSlot.HEAD, itemStack);
+                            armorStandEntity.equipStack(EquipmentSlot.MAINHAND, itemStack);
+                            armorStandEntity.equipStack(EquipmentSlot.OFFHAND, itemStack);
+                            armorStandEntity.setShowArms(true);
+                            armorStandEntity.setYaw(180F);
+                            armorStandEntity.setNoGravity(true);
+                            armorStandEntity.setCustomName(Text.of("Plush"));
+                            world.spawnEntity(armorStandEntity);
+                        }
+                    }
+                    plushies++;
+                }
+            }
+        }
+        return 1;
+    }
+
+}
