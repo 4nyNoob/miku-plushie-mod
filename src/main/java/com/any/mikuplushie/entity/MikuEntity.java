260f1c014f24bf5771f134261bbb574c09eb2660
any <aniziooliveira9@gmail.com>
2025-11-26 19:58:55 -0300

    added miku attack animation and basic AI goals

--- src/main/java/com/any/mikuplushie/entity/MikuEntity.java
+++ src/main/java/com/any/mikuplushie/entity/MikuEntity.java
@@ -5,20 +5,20 @@ import com.any.mikuplushie.ModSoundEvents;
 import com.any.mikuplushie.entity.variant.MikuVariant;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.goal.FollowOwnerGoal;
-import net.minecraft.entity.ai.goal.LookAtEntityGoal;
-import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
 import net.minecraft.entity.mob.MobEntity;
+import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.passive.TameableEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
@@ -52,8 +52,12 @@ public class MikuEntity extends TameableEntity implements GeoEntity {
     private static final RawAnimation DANCE3 = RawAnimation.begin().thenLoop("misc.dance3");
     private static final RawAnimation DANCE4 = RawAnimation.begin().thenLoop("misc.dance4");
     private static final RawAnimation DANCE5 = RawAnimation.begin().thenLoop("misc.dance5");
+    private static final RawAnimation SWIPE = RawAnimation.begin().thenPlay("attack.swipe");
+    private static final RawAnimation SWIPE2 = RawAnimation.begin().thenPlay("attack.swipe2");
+    private static final RawAnimation SWIPE3 = RawAnimation.begin().thenPlay("attack.swipe3");
 
     private static RawAnimation SELECTED_DANCE = DANCE;
+    private static RawAnimation SELECTED_ATTACK = SWIPE;
 
     private boolean songPlaying;
     @Nullable
@@ -67,27 +71,44 @@ public class MikuEntity extends TameableEntity implements GeoEntity {
     @Override
     public void initGoals() {
         this.goalSelector.add(0, new SwimGoal(this));
-        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 16F));
-        this.goalSelector.add(2, new LookAtEntityGoal(this, MikuEntity.class, 8F));
-        this.goalSelector.add(2, new FollowOwnerGoal(this,1.0F, 5F, 1F, true));
+        this.goalSelector.add(1, new SitGoal(this));
+        this.goalSelector.add(2, new MikuDelayedAttackGoal(this, 1.5F, true));
+        this.goalSelector.add(4, new FollowOwnerGoal(this,1.0F, 5F, 1F, true));
+        this.goalSelector.add(5, new TemptGoal(this, 1.5, Ingredient.ofItems(ModItems.CANUDINHO), false));
+        this.goalSelector.add(6, new LookAtEntityGoal(this, MikuEntity.class, 8F));
+        this.goalSelector.add(7, new LookAtEntityGoal(this, PlayerEntity.class, 8F));
+        this.goalSelector.add(8, new LookAroundGoal(this));
+        this.targetSelector.add(1, new TrackOwnerAttackerGoal(this));
+        this.targetSelector.add(2, new AttackWithOwnerGoal(this));
     }
 
     public static DefaultAttributeContainer.Builder createAttributes() {
         return MobEntity.createMobAttributes()
-            .add(EntityAttributes.GENERIC_MAX_HEALTH, 8.0)
-            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3F);
+            .add(EntityAttributes.GENERIC_MAX_HEALTH, 10.0F)
+            .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.3F)
+            .add(EntityAttributes.GENERIC_ATTACK_DAMAGE, 2.0F);
     }
 
     //GECKO LIB STUFF
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        controllers.add(new AnimationController<>(this, "Miku", state -> {
+        controllers.add(new AnimationController<>(this, "Miku", 2, state -> {
             if (MikuEntity.this.isInSittingPose()){
-                return  state.setAndContinue(MikuEntity.this.isSongPlaying() ? SIT_DANCE : SIT);
+                return state.setAndContinue(MikuEntity.this.isSongPlaying() ? SIT_DANCE : SIT);
             } else {
-                return  state.setAndContinue(MikuEntity.this.isSongPlaying() ? SELECTED_DANCE : IDLE);
+
+                if (MikuEntity.this.isSongPlaying()){
+                    return state.setAndContinue(SELECTED_DANCE);
+                } else {
+                    if (MikuEntity.this.handSwinging){
+                        return state.setAndContinue(SELECTED_ATTACK);
+                    } else {
+                        return state.setAndContinue(IDLE);
+                    }
+                }
             }
         }));
+
     }
 
     @Override
@@ -106,6 +127,61 @@ public class MikuEntity extends TameableEntity implements GeoEntity {
     }
 
     @Override
+    public final int getHandSwingDuration() {
+        return 10;
+    }
+
+    @Override
+    public void swingHand(Hand hand) {
+        super.swingHand(hand);
+        int randomAttack = this.random.nextBetween(1, 3);
+        switch (randomAttack) {
+            case 1: SELECTED_ATTACK = SWIPE; break;
+            case 2: SELECTED_ATTACK = SWIPE2; break;
+            case 3: SELECTED_ATTACK = SWIPE3; break;
+        }
+    }
+
+    //MIKU DELAYED ATTACK GOAL
+    static class MikuDelayedAttackGoal extends MeleeAttackGoal {
+        private int attackDelay = 5;
+        private LivingEntity target;
+        private boolean mikuAttacking;
+
+        public MikuDelayedAttackGoal(PathAwareEntity mob, double speed, boolean pauseWhenMobIdle) {
+            super(mob, speed, pauseWhenMobIdle);
+        }
+        /**
+        *   Swing the entity hand but handle the actual attack on the {@link  com.any.mikuplushie.entity.MikuEntity.MikuDelayedAttackGoal#tick()} method
+        */
+        protected void attack(LivingEntity target, double squaredDistance) {
+            double d = this.getSquaredMaxAttackDistance(target);
+            if (squaredDistance <= d && this.isCooledDown()) {
+                this.resetCooldown();
+                this.mob.swingHand(Hand.MAIN_HAND);
+                this.target = target;
+                this.mikuAttacking = true;
+            }
+        }
+        /**
+        *   Receive the attack command but wait 5 ticks to execute it, then reset
+        */
+        @Override
+        public void tick() {
+            if (this.mikuAttacking){
+                --this.attackDelay;
+                if (this.attackDelay < 0){
+                    this.mob.tryAttack(this.target);
+                    this.mikuAttacking = false;
+                }
+            } else {
+                this.attackDelay = 5;
+            }
+            super.tick();
+        }
+    }
+
+    @Override
     public @Nullable PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
         return null;
     }
@@ -118,11 +194,13 @@ public class MikuEntity extends TameableEntity implements GeoEntity {
     @Override
     protected void initDataTracker() {
         super.initDataTracker();
+        //TRACK MIKU VARIANT BETWEEN SERVER AND CLIENT
         this.dataTracker.startTracking(MIKU_VARIANT, 0);
     }
 
     @Override
     public EntityData initialize(ServerWorldAccess world, LocalDifficulty difficulty, SpawnReason spawnReason, @Nullable EntityData entityData, @Nullable NbtCompound entityNbt) {
+        //INITIALIZE MIKU WITH A RANDOM VARIANT
         MikuVariant variant = Util.getRandom(MikuVariant.values(), this.random);
         setVariant(variant);
         return super.initialize(world, difficulty, spawnReason, entityData, entityNbt);
@@ -135,6 +213,7 @@ public class MikuEntity extends TameableEntity implements GeoEntity {
 
     @Override
     public float getSoundPitch() {
+        //FIXED SOUND PITCH
         return 1F;
     }
 
@@ -195,15 +274,15 @@ public class MikuEntity extends TameableEntity implements GeoEntity {
         }
 
         super.tickMovement();
-
+        this.tickHandSwing();
     }
 
     @Override
     public void setNearbySongPlaying(BlockPos songPosition, boolean playing) {
         this.songSource = songPosition;
         this.songPlaying = playing;
-        Random random = this.random;
-        int randomDance = random.nextBetweenExclusive(1, 5);
+        Random rand = this.random;
+        int randomDance = rand.nextBetweenExclusive(1, 5);
         switch (randomDance) {
             case 1: SELECTED_DANCE = DANCE; break;
             case 2: SELECTED_DANCE = DANCE2; break;
