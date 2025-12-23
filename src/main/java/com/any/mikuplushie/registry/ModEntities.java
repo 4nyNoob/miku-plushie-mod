7842e1292493ab4df71a2b7b05dd2a818dc33e03
any <aniziooliveira9@gmail.com>
2025-12-22 22:21:04 -0300

    automate attribute registries for plush entities

--- src/main/java/com/any/mikuplushie/registry/ModEntities.java
+++ src/main/java/com/any/mikuplushie/registry/ModEntities.java
@@ -2,10 +2,12 @@ package com.any.mikuplushie.registry;
 
 import com.any.mikuplushie.MikuPlushie;
 import com.any.mikuplushie.entity.*;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.registry.Registries;
@@ -17,6 +19,8 @@ import java.util.List;
 
 public class ModEntities {
 
+
+    public static List<EntityType<?>> PLUSH_ENTITIES = new ArrayList<>();
 
     private static final float PLUSH_WIDTH = 0.6F;
     private static final float PLUSH_HEIGHT = 0.8F;
@@ -34,23 +38,18 @@ public class ModEntities {
 
 
     public static <T extends MobEntity> EntityType<T> registerMob(String name, EntityType.EntityFactory<T> entity) {
-        return Registry.register(Registries.ENTITY_TYPE,
+        EntityType<T> entityType = Registry.register(Registries.ENTITY_TYPE,
             new Identifier(MikuPlushie.MOD_ID, name), FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, entity)
                 .dimensions(EntityDimensions.fixed(PLUSH_WIDTH, PLUSH_HEIGHT)).build());
+        PLUSH_ENTITIES.add(entityType);
+        return entityType;
     }
 
     public static void initialize(){
         MikuPlushie.LOGGER.info("Registering " + MikuPlushie.MOD_ID + " Entities");
-
-        FabricDefaultAttributeRegistry.register(MIKU, MikuEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(TETO, TetoEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(AIKO, AikoEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(NERU, NeruEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(RIN, RinEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(LEN, LenEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(KONOHA, KonohaEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(LUKA, LukaEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(MEIKO, MeikoEntity.createAttributes());
-        FabricDefaultAttributeRegistry.register(GUMI, GumiEntity.createAttributes());
+        for (EntityType<?> entity : PLUSH_ENTITIES) {
+            //noinspection unchecked
+            FabricDefaultAttributeRegistry.register((EntityType<? extends AbstractPlushEntity>) entity, AbstractPlushEntity.createAttributes());
+        }
     }
 }
