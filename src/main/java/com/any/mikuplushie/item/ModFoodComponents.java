b6d9af810ad47fd07f7cbc17e646578be81c95ef
any <aniziooliveira9@gmail.com>
2025-12-24 19:01:12 -0300

    buffed baguette and leek hunger recovery

--- src/main/java/com/any/mikuplushie/item/ModFoodComponents.java
+++ src/main/java/com/any/mikuplushie/item/ModFoodComponents.java
@@ -4,6 +4,6 @@ import net.minecraft.item.FoodComponent;
 import net.minecraft.item.FoodComponents;
 
 public class ModFoodComponents extends FoodComponents {
-    public static final FoodComponent BAGUETTE = new FoodComponent.Builder().hunger(6).saturationModifier(0.6F).build();
-    public static final FoodComponent LEEK = new FoodComponent.Builder().hunger(2).saturationModifier(0.6F).build();
+    public static final FoodComponent BAGUETTE = new FoodComponent.Builder().hunger(8).saturationModifier(0.6F).build();
+    public static final FoodComponent LEEK = new FoodComponent.Builder().hunger(4).saturationModifier(0.6F).build();
 }
