40b55bbc09677248c56932e0cdde39530fe68c88
any <aniziooliveira9@gmail.com>
2026-01-14 00:18:52 -0300

    dynamically create Sound Events

--- src/main/java/com/any/mikuplushie/registry/ModSoundEvents.java
+++ src/main/java/com/any/mikuplushie/registry/ModSoundEvents.java
@@ -1,81 +1,23 @@
 package com.any.mikuplushie.registry;
 
 import com.any.mikuplushie.MikuPlushie;
+import com.any.mikuplushie.entity.KonohaEntity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ModSoundEvents {
 	private ModSoundEvents() {
 	}
 
-
-	public static List<SoundEvent> MIKU_PLUSHIES_SOUND_EVENTS = List.of(
-		//MIKU
-		registerSound("miku_canudinho"),
-		registerSound("miku_oie"),
-		registerSound("miku_dor"),
-		registerSound("miku_bye"),
-		registerSound("miku_equip"),
-		registerSound("miku_eat"),
-
-		//AIKO
-		registerSound("aiko_oie"),
-		registerSound("aiko_dor"),
-		registerSound("aiko_bye"),
-		registerSound("aiko_equip"),
-
-		//TETO
-		registerSound("teto_oie"),
-		registerSound("teto_dor"),
-		registerSound("teto_bye"),
-		registerSound("teto_equip"),
-
-		//AKITA NERU
-		registerSound("akita_oie"),
-		registerSound("akita_dor"),
-		registerSound("akita_bye"),
-		registerSound("akita_equip"),
-
-		//RIN
-		registerSound("rin_oie"),
-		registerSound("rin_dor"),
-		registerSound("rin_bye"),
-		registerSound("rin_equip"),
-
-		//LEN
-		registerSound("len_oie"),
-		registerSound("len_dor"),
-		registerSound("len_bye"),
-		registerSound("len_equip"),
-
-		//LUKA
-		registerSound("luka_oie"),
-		registerSound("luka_dor"),
-		registerSound("luka_bye"),
-		registerSound("luka_equip"),
-
-		//MEIKO
-		registerSound("meiko_oie"),
-		registerSound("meiko_dor"),
-		registerSound("meiko_bye"),
-		registerSound("meiko_equip"),
-
-		//GUMI
-		registerSound("gumi_oie"),
-		registerSound("gumi_dor"),
-		registerSound("gumi_bye"),
-		registerSound("gumi_equip"),
-
-		//KAITO
-		registerSound("kaito_oie"),
-		registerSound("kaito_dor"),
-		registerSound("kaito_bye"),
-		registerSound("kaito_equip")
-	);
+	public static List<SoundEvent> MIKU_PLUSHIES_SOUND_EVENTS = new ArrayList<>();
+	protected static List<String> SOUND_EVENT = List.of("oie", "dor", "bye", "equip");
+	protected static List<String> MIKU_SOUND_EVENT = List.of("canudinho", "eat");
 
 	private static SoundEvent registerSound(String id) {
 		Identifier identifier = Identifier.of(MikuPlushie.MOD_ID, id);
@@ -84,5 +26,32 @@ public class ModSoundEvents {
 
 	public static void initialize() {
 		MikuPlushie.LOGGER.info("Registering " + MikuPlushie.MOD_ID + " Sounds");
+
+		//DYNAMICALLY REGISTER SOUND EVENTS
+		for (EntityType<?> plush : ModEntities.PLUSH_ENTITIES){
+
+			//SKIP KONOHA PLUSH
+			if (!plush.equals(ModEntities.KONOHA)){
+
+				//ADD MIKU SOUND EVENTS
+				if (plush.equals(ModEntities.MIKU)){
+					for (String mikuSoundEvent : MIKU_SOUND_EVENT){
+						SoundEvent soundEvent = registerSound(plush.getUntranslatedName().replace("_plush", "") + "_" + mikuSoundEvent);
+						MIKU_PLUSHIES_SOUND_EVENTS.add(soundEvent);
+					}
+				}
+
+				//ADD REGULAR SOUND EVENTS
+				for (String event : SOUND_EVENT){
+					SoundEvent soundEvent = registerSound(plush.getUntranslatedName().replace("_plush", "") + "_" + event);
+					MIKU_PLUSHIES_SOUND_EVENTS.add(soundEvent);
+				}
+			}
+		}
+
+		//PRINT SOUND EVENTS
+		for (SoundEvent soundEvent : MIKU_PLUSHIES_SOUND_EVENTS){
+			System.out.println(soundEvent.getId());
+		}
 	}
 }
