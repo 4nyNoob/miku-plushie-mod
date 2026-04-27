e693830d13a9e1ad14aa4b796e0c65db92d8c7d6
any <aniziooliveira9@gmail.com>
2026-04-27 19:53:57 -0300

    update mod name

--- common/src/main/java/com/example/examplemod/Constants.java
+++ common/src/main/java/com/example/examplemod/Constants.java
@@ -6,6 +6,6 @@ import org.slf4j.LoggerFactory;
 public class Constants {
 
 	public static final String MOD_ID = "miku_plushie";
-	public static final String MOD_NAME = "ExampleMod";
+	public static final String MOD_NAME = "MikuPlushies";
 	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
--- neoforge/src/main/java/com/example/examplemod/MikuPlushie.java
+++ neoforge/src/main/java/com/example/examplemod/MikuPlushie.java
@@ -5,9 +5,9 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
 
 @Mod(Constants.MOD_ID)
-public class ExampleMod {
+public class MikuPlushie {
 
-    public ExampleMod(IEventBus eventBus) {
+    public MikuPlushie(IEventBus eventBus) {
 
         // This method is invoked by the NeoForge mod loader when it is ready
         // to load your mod. You can access NeoForge and Common code in this
